4a7de9b29974ac2b95bc6d1ec79c7e53965680f2
Hiroki Yamamoto <hymt73@gmail.com>
2022-05-24 18:20:21 +0200

    working on 2nd cmd

--- src/main.c
+++ src/main.c
@@ -43,23 +43,6 @@ char	*is_cmd_exist_and_executable(char *cmd)
 
 int	main(int argc, char *argv[])
 {
-	// -- Execve --
-	/*
-	path to linux commands execution files.
-		/bin
-		/usr/bin
-	*/
-	// const char *path = "/bin/ls";
-	// char *const args[] = {"/bin/ls", "-lh"};
-	// const char *path = "/bin/date";
-	// char *const args[] = {"/bin/date", "-u"};
-	// const char *path = "/bin/cat";
-	// char *const args[] = {"/bin/cat", "a.txt"};
-	// execve(path, args, NULL);
-
-	char	*i_file;
-	int		pid;
-
 	int		i;
 	char	*args_first[ARG_MAX];
 	char	**cmd_first;
@@ -71,11 +54,14 @@ int	main(int argc, char *argv[])
 	char	*args_second[ARG_MAX];
 	char	**cmd_second;
 	int		argnum_second;
+	char	*path_second;
 
 	if (argc == 5)
 	{
+		//-- input file --
 		is_file_exist_and_readable(argv[1]);
 
+		//-- first cmd --
 		argnum_first = count_num_of_strings(argv[2]);
 		if (argnum_first == 0)
 		{
@@ -93,7 +79,6 @@ int	main(int argc, char *argv[])
 			exit(1);
 		if (pipe(p) < 0)
 			exit(1);
-		
 		if (fork() == 0)
 		{
 			dup2(p[1], 1);
@@ -101,11 +86,34 @@ int	main(int argc, char *argv[])
 		}
 		else
 			printf("else\n");
+		// char inbuf[20];
+		// read(p[0], inbuf, 20);
+		//printf("read: %s\n", inbuf);
 
-		char inbuf[20];
-		read(p[0], inbuf, 20);
-		printf("read: %s\n", inbuf);
+		//-- second cmd --
+		argnum_second = count_num_of_strings(argv[3]);
+		if (argnum_second == 0)
+		{
+			write(1, "Error: Please provide a command.\n", 33);
+			exit(1);
+		}
+		cmd_second = cust_split(argv[2]);
+		i = -1;
+		while (++i < argnum_second)
+			args_second[i] = cmd_second[i];
+		args_second[argnum_first] = NULL;
+		path_second = is_cmd_exist_and_executable(args_second[0]);
+		if (path_second == NULL)
+			exit(1);
+		if (fork() == 0)
+		{
+			dup2(0, p[0]);
+			execve(path_second, args_second, NULL);
+		}
+		else
+			printf("else\n");
 
+		//-- output file --
 		// if (argv[4])
 		// {
 		// 	error handle when you couldn't create it or some other staffs.
@@ -117,5 +125,5 @@ int	main(int argc, char *argv[])
 	return (0);
 }
 
-
-//TODO: use perror or strerror and shows error message based on errono?
+//TODO: learn how to pass output to 2nd cmd & how to create output file with allowed funcs.
+//ref: https://gist.github.com/iximiuz/65c7d2d128c374ef83d885dfef74bed7
