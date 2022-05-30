91bfe105ae7543b91802ce91eb6ddb9d5289b061
Hiroki Yamamoto <hymt73@gmail.com>
2022-05-30 13:19:24 +0200

    added error handle for dup2 fucntion

--- mandatory/src/main.c
+++ mandatory/src/main.c
@@ -6,18 +6,46 @@
 /*   By: hiyamamo <hiyamamo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 14:57:25 by hiyamamo          #+#    #+#             */
-/*   Updated: 2022/05/30 11:37:03 by hiyamamo         ###   ########.fr       */
+/*   Updated: 2022/05/30 13:18:46 by hiyamamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	exec_first_cmd(char *argv[], int *p, int pid, int fd)
+void	first_cmd_child(int *p, char **args, char *input_file)
 {
-	char	*args[100]; // char	*args[ARG_MAX];
+	int	fd;
+
+	close(p[0]);
+	if (dup2(p[1], 1) == -1)
+		cust_perror("Error");
+	fd = open(input_file, O_RDONLY);
+	if (dup2(fd, 0) == -1)
+		cust_perror("Error");
+	close(fd);
+	execve(is_cmd_exist_and_executable(args[0]), args, NULL);
+}
+
+void	second_cmd_child(int *p, char **args, char *output_file)
+{
+	int	fd;
+
+	close(p[1]);
+	if (dup2(p[0], 0) == -1)
+		cust_perror("Error");
+	fd = open(output_file, O_CREAT | O_TRUNC | O_WRONLY, 0777);
+	if (dup2(fd, 1) == -1)
+		cust_perror("Error");
+	close(fd);
+	execve(is_cmd_exist_and_executable(args[0]), args, NULL);
+}
+
+void	exec_first_cmd(char *argv[], int *p)
+{
+	char	*args[ARG_MAX];
 	char	**cmd;
 	int		arg_num;
-	char	*path;
+	int		pid;
 
 	arg_num = count_num_of_strings(argv[2]);
 	if (arg_num == 0)
@@ -26,27 +54,19 @@ void	exec_first_cmd(char *argv[], int *p, int pid, int fd)
 	args[arg_num] = NULL;
 	while (0 <= --arg_num)
 		args[arg_num] = cmd[arg_num];
-	path = is_cmd_exist_and_executable(args[0]);
 	pid = fork();
 	if (pid < 0)
 		cust_perror("Error");
 	if (pid == 0)
-	{
-		close(p[0]);
-		dup2(p[1], 1);
-		fd = open(argv[1], O_RDONLY);
-		dup2(fd, 0);
-		close(fd);
-		execve(path, args, NULL);
-	}
+		first_cmd_child(p, args, argv[1]);
 }
 
-void	exec_second_cmd(char *argv[], int *p, int pid, int fd)
+void	exec_second_cmd(char *argv[], int *p)
 {
-	char	*args[100]; // char	*args[ARG_MAX];
+	char	*args[ARG_MAX];
 	char	**cmd;
 	int		arg_num;
-	char	*path;
+	int		pid;
 
 	arg_num = count_num_of_strings(argv[3]);
 	if (arg_num == 0)
@@ -55,36 +75,26 @@ void	exec_second_cmd(char *argv[], int *p, int pid, int fd)
 	args[arg_num] = NULL;
 	while (0 <= --arg_num)
 		args[arg_num] = cmd[arg_num];
-	path = is_cmd_exist_and_executable(args[0]);
 	pid = fork();
 	if (pid < 0)
 		cust_perror("Error");
 	if (pid == 0)
-	{
-		close(p[1]);
-		dup2(p[0], 0);
-		fd = open(argv[4], O_CREAT | O_TRUNC | O_WRONLY, 0777);
-		dup2(fd, 1);
-		close(fd);
-		execve(path, args, NULL);
-	}
+		second_cmd_child(p, args, argv[4]);
 }
 
 int	main(int argc, char *argv[])
 {
 	int	p[2];
-	int	pid;
-	int	fd;
 
 	if (argc == 5)
 	{
 		is_file_exist_and_readable(argv[1]);
 		if (pipe(p) < 0)
 			cust_perror("Error");
-		exec_first_cmd(argv, p, pid, fd);
+		exec_first_cmd(argv, p);
 		if (waitpid(-1, NULL, 0) == -1)
 			cust_perror("Error");
-		exec_second_cmd(argv, p, pid, fd);
+		exec_second_cmd(argv, p);
 		close(p[0]);
 		close(p[1]);
 		if (waitpid(-1, NULL, 0) == -1)
